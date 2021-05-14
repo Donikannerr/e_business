5bcb8aedae880ce65be878a30e4f2f745589ae20
Donikannerr <67806377+Donikannerr@users.noreply.github.com>
2021-05-15 00:34:54 +0200

    add appbar_static

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:e_business/my_app.dart';
 import 'package:flutter/material.dart';
 
-//test 
 
 void main() {
   runApp(MyApp());
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(179, 163, 203, 1),
+        title: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.menu),
+              Text.rich(
+                TextSpan(
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                  children: [
+                    WidgetSpan(
+                      child: Icon(Icons.public),
+                    ),
+                    TextSpan(
+                      text: 'ffline - Your dopamin detox',
+                    )
+                  ],
+                ),
+              ),
+              Icon(Icons.share)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
